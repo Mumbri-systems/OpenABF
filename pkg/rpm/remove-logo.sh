969b54abde73a897e99d49e645d73124218aadbe
mumbricht <49099056+mumbricht@users.noreply.github.com>
2026-04-28 13:47:11 +0200

    act directly on extracted files

--- pkg/rpm/remove-logo.sh
+++ pkg/rpm/remove-logo.sh
@@ -2,11 +2,8 @@
 
 VERSION='3c1b52a02a15007d3dcb7746b08a4f3db0e7a0b6'
 
-tar -xzvf OpenABF-$VERSION.tar.gz
 rm OpenABF-$VERSION/logo.png
 rm OpenABF-$VERSION/docs/images/*
 sed -i -e 's/logo.png//' OpenABF-$VERSION/src/Makefile
 sed -i -e 's/banner.svg//' OpenABF-$VERSION/src/Makefile
 sed -i -e 's/logo.svg//' OpenABF-$VERSION/src/Makefile
-
-tar -czvf OpenABF-$VERSION.tar.gz OpenABF-$VERSION
