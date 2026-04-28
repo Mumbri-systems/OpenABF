b33aca331c5446835abc63dc099237ae4568b7a4
mumbricht <49099056+mumbricht@users.noreply.github.com>
2026-04-28 12:14:53 +0200

    remove other instances of logo
    Remove logo files and references from the OpenABF package.

--- pkg/rpm/remove-logo.sh
+++ pkg/rpm/remove-logo.sh
@@ -4,6 +4,9 @@ VERSION='3c1b52a0'
 
 tar -xzvf openabf-$VERSION.tar.gz
 rm OpenABF-$VERSION/logo.png
+rm OpenABF-$VERSION/docs/images/*
 sed -i -e 's/logo.png//' OpenABF-$VERSION/src/Makefile
+sed -i -e 's/banner.svg//' OpenABF-$VERSION/src/Makefile
+sed -i -e 's/logo.svg//' OpenABF-$VERSION/src/Makefile
 
 tar -czvf openabf-$VERSION.tar.gz OpenABF-$VERSION
