ce233d9725ebfca93b71f4721c820aaca58955aa
mumbricht <49099056+mumbricht@users.noreply.github.com>
2026-06-22 10:04:26 +0200

    fix version/tag missmatch

--- pkg/rpm/remove-logo.sh
+++ pkg/rpm/remove-logo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-VERSION='v2.1.0'
+VERSION='2.1.0'
 # SHORTVERSION='3c1b52a'
-wget https://github.com/educelab/OpenABF/archive/$VERSION/OpenABF-$VERSION.tar.gz
-tar -xf $VERSION.tar.gz
+wget https://github.com/educelab/OpenABF/archive/v$VERSION/OpenABF-v$VERSION.tar.gz
+tar -xf v$VERSION.tar.gz
 #remove prohibited copyright content
 rm OpenABF-$VERSION/logo.png
 rm OpenABF-$VERSION/docs/images/*
